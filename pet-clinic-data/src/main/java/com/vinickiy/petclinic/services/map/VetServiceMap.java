e9dea32a5f455a9ee1e109a0cd395e0fbb3c2e75
SergeyV1 <sergey.vinickiy@verifone.com>
2020-04-10 13:34:53 +0300

    Create specialties, add to Vets on Bootstrup closes #39

--- pet-clinic-data/src/main/java/com/vinickiy/petclinic/services/map/VetServiceMap.java
+++ pet-clinic-data/src/main/java/com/vinickiy/petclinic/services/map/VetServiceMap.java
@@ -1,6 +1,8 @@
 package com.vinickiy.petclinic.services.map;
 
+import com.vinickiy.petclinic.model.Specialty;
 import com.vinickiy.petclinic.model.Vet;
+import com.vinickiy.petclinic.services.SpecialtyService;
 import com.vinickiy.petclinic.services.VetService;
 import org.springframework.stereotype.Service;
 
@@ -9,6 +11,12 @@ import java.util.Set;
 
 @Service
 public class VetServiceMap extends AbstractMapService<Vet, Long> implements VetService {
+
+    private final SpecialtyService specialtyService;
+
+    public VetServiceMap(SpecialtyService specialtyService) {
+        this.specialtyService = specialtyService;
+    }
 
     @Override
     public Set<Vet> findAll() {
@@ -27,6 +35,14 @@ public class VetServiceMap extends AbstractMapService<Vet, Long> implements VetS
 
     @Override
     public Vet save(Vet object) {
+        if(object.getSpecialties().size() > 0){
+            object.getSpecialties().forEach(specialty -> {
+                if (specialty.getId() == null) {
+                    Specialty savedSpecialty = specialtyService.save(specialty);
+                    specialty.setId(savedSpecialty.getId());
+                }
+            });
+        }
         return super.save(object);
     }
 
