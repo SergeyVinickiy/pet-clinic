42ffb48132a07eb2ad19063323610f4b6a18c8b2
SergeyV1 <sergey.vinickiy@verifone.com>
2020-04-13 18:55:04 +0300

    Update profiles to use Spring Data JPA Service closes #74

--- pet-clinic-data/src/main/java/com/vinickiy/petclinic/services/map/PetMapService.java
+++ pet-clinic-data/src/main/java/com/vinickiy/petclinic/services/map/PetMapService.java
@@ -2,12 +2,14 @@ package com.vinickiy.petclinic.services.map;
 
 import com.vinickiy.petclinic.model.Pet;
 import com.vinickiy.petclinic.services.PetService;
+import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Service;
 
 import java.util.Set;
 
 
 @Service
+@Profile({"default", "map"})
 public class PetMapService extends AbstractMapService<Pet, Long> implements PetService {
 
     @Override
--- pet-clinic-data/src/main/java/com/vinickiy/petclinic/services/map/VetMapService.java
+++ pet-clinic-data/src/main/java/com/vinickiy/petclinic/services/map/VetMapService.java
@@ -4,12 +4,14 @@ import com.vinickiy.petclinic.model.Specialty;
 import com.vinickiy.petclinic.model.Vet;
 import com.vinickiy.petclinic.services.SpecialtyService;
 import com.vinickiy.petclinic.services.VetService;
+import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Service;
 
 import java.util.Set;
 
 
 @Service
+@Profile({"default", "map"})
 public class VetMapService extends AbstractMapService<Vet, Long> implements VetService {
 
     private final SpecialtyService specialtyService;
