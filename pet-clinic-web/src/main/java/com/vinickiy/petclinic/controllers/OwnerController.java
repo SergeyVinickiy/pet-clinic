f1dc689a07f1d77ede85c5099837f5778e7ff051
SergeyV1 <sergey.vinickiy@verifone.com>
2020-04-19 21:56:44 +0300

    adding WebDataBinder

--- pet-clinic-web/src/main/java/com/vinickiy/petclinic/controllers/OwnerController.java
+++ pet-clinic-web/src/main/java/com/vinickiy/petclinic/controllers/OwnerController.java
@@ -4,7 +4,9 @@ package com.vinickiy.petclinic.controllers;
 import com.vinickiy.petclinic.services.OwnerService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.WebDataBinder;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.InitBinder;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
@@ -25,6 +27,11 @@ public class OwnerController {
         model.addAttribute("owners", ownerService.findAll());
 
         return "owners/index";
+    }
+
+    @InitBinder
+    public void setAllowedFields(WebDataBinder dataBinder) {
+        dataBinder.setDisallowedFields("id");
     }
 
     @RequestMapping({"/find"})
