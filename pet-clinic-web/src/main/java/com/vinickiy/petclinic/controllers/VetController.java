73e66da3e3de68f22f925f44e9eb9a1a0d0f9f8f
SergeyV1 <sergey.vinickiy@verifone.com>
2020-04-24 15:06:23 +0300

    Implement Vets Json Endpoint closes #92

--- pet-clinic-web/src/main/java/com/vinickiy/petclinic/controllers/VetController.java
+++ pet-clinic-web/src/main/java/com/vinickiy/petclinic/controllers/VetController.java
@@ -1,10 +1,15 @@
 package com.vinickiy.petclinic.controllers;
 
 
+import com.vinickiy.petclinic.model.Vet;
 import com.vinickiy.petclinic.services.VetService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.Set;
 
 @Controller
 public class VetController {
@@ -20,6 +25,11 @@ public class VetController {
         model.addAttribute("vets", vetService.findAll());
 
         return "vets/index";
+    }
+
+    @GetMapping("/api/vets")
+    public @ResponseBody Set<Vet> getVetsJson(){
+        return vetService.findAll();
     }
 
 }
